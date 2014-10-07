108f1125c376f25c98f1a49e11c48f9aa75854f4
Ali Hawili <ali.hawili@gmail.com>
2014-10-08 00:26:41 +0300

    Challange done

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultField: UILabel!
+    @IBOutlet weak var entryTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertAgeToDogYears(sender: UIButton) {
+        resultField.hidden = false
+        var result = Double(entryTextField.text.toInt()!)
+        if result > 2 {
+            result = 10.5*2.0 + (result-2)*4.0
+        } else {
+            result *= 10.5
+        }
+        resultField.text = "\(result)"
+        entryTextField.resignFirstResponder()
+    }
 
 }
 
